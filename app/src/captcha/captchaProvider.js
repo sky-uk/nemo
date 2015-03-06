27f8643632bc2d66642a584bf2922c4df36ea4a0
WebTeam <dl-nowtv-webteam@bskyb.com>
2015-03-06 11:18:36 +0000

    @ricardo @adam adding focus state to the captcha provider in order to be able to normalise the focus handling at the root tag of the component.

--- app/src/captcha/captchaProvider.js
+++ app/src/captcha/captchaProvider.js
@@ -1,9 +1,9 @@
 angular.module('nemo').provider('captcha', [function () {
     return {
-        template: '<div class="nemo-captcha">' +
+        template: '<div class="nemo-captcha" ng-class="{ focus: isFocused }">' +
             '<img class="nemo-captcha-img" ng-src="{{captchaModel.getImageUri()}}">' +
             '<div class="nemo-captcha-play" ng-click="playAudio($event)"></div>' +
-            '<input class="nemo-captcha-input" type="text" ng-model="model.value" name="captchaInput">' +
+            '<input class="nemo-captcha-input" type="text" ng-model="model.value" name="captchaInput" ng-focus="setFocus()" ng-blur="releaseFocus()">' +
             '<div class="nemo-captcha-refresh" ng-click="refreshCaptcha($event)">{{getRequestCaptchaCopy()}}</div>' +
             '<audio class="nemo-captcha-audio" ng-src="{{captchaModel.getAudioUri()}}">' +
                 'Your browser does not support audio' +
@@ -27,6 +27,14 @@ angular.module('nemo').provider('captcha', [function () {
                 $event.stopPropagation();
                 $event.preventDefault();
                 element.find('audio')[0].play();
+            };
+
+            scope.setFocus = function () {
+                scope.isFocused = true;
+            };
+
+            scope.releaseFocus = function () {
+                scope.isFocused = false;
             };
         },
         controller: 'CaptchaCtrl',
