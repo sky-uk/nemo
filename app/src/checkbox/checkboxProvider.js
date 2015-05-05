080027672531957e1500bd030f1e576a5a92f0f5
Web Team <dl-nowtv-webteam@bskyb.com>
2015-05-05 17:20:05 +0100

    @barry @ricardo refactoring the checkbox comnponent to have a better control on its UI / UX

--- app/src/checkbox/checkboxProvider.js
+++ app/src/checkbox/checkboxProvider.js
@@ -1,15 +1,55 @@
 angular.module('nemo').provider('checkbox', [function () {
     return {
-        template: '<input type="checkbox" ng-click="setActiveCheckboxField()" />',
+        template: '<div data-ng-click="toggle()" data-ng-keyup="toggleIfEnter($event)" data-ng-class="{checked: isChecked, focused: isFocused()}">' +
+        '<label class="tick" data-ng-show="isChecked">\u2714</label>' +
+        '<input type="text" data-ng-focus="setFocus()" data-ng-blur="releaseFocus()" ' +
+            'style="position: absolute; top: 0; left: 0; width: 0; height: 0; opacity: 0; cursor: pointer; font-size: 0; color: transparent; text-indent: 100%; padding: 0; border: none;" />' +
+        '</div>',
         defaultValue: false,
         linkFn: function (scope, element, attrs, controllers) {
+
             var ngModelCtrl = controllers[0],
                 formHandlerCtrl = controllers[1],
-                fieldValue = scope.model.value;
-            formHandlerCtrl.setFieldValue(scope.model.name, fieldValue === true || fieldValue === 'true');
-            scope.setActiveCheckboxField = function () {
-                ngModelCtrl.$setTouched();
-                scope.setActiveField();
+                fieldValue = scope.model.value,
+                fieldName = scope.model.name,
+                hasGenuineFocus = false;
+
+            setValue(fieldValue === true || fieldValue === 'true');
+
+            scope.isFocused = function () {
+                return hasGenuineFocus && ngModelCtrl.isActive;
+            };
+
+            scope.toggle = function () {
+                setValue(!scope.isChecked);
+                scope.setFocus();
+                formHandlerCtrl.setFieldDirtyTouched(fieldName);
+            };
+
+            scope.toggleIfEnter = function ($event) {
+                var spaceKeyCode = 32;
+                if ($event.which === spaceKeyCode) {
+                    scope.toggle();
+                }
+            };
+
+            scope.setFocus = function () {
+                hasGenuineFocus = true;
+                setActiveState();
+            };
+
+            scope.releaseFocus = function () {
+                hasGenuineFocus = false;
+                formHandlerCtrl.setFieldDirtyTouched(fieldName);
+            };
+
+            function setValue(value) {
+                scope.isChecked = value;
+                formHandlerCtrl.setFieldValue(fieldName, value);
+            }
+
+            function setActiveState() {
+                formHandlerCtrl.setActiveField(fieldName);
             }
         },
         $get: angular.noop
