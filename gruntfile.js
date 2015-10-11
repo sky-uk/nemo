fec2df5b853df5f0e0c8c2ea7e98a463c54244a1
popcorn <ricardo.mallols@bskyb.com>
2015-10-11 19:34:08 +0100

    @Ricardo Sorting out grunt-contrib dependencies

--- gruntfile.js
+++ gruntfile.js
@@ -57,8 +57,8 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-express');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-express');
 
 	grunt.registerTask('build', ['concat']);
 	grunt.registerTask('build-and-min', ['build', 'uglify']);
